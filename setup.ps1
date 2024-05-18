fa8a7593ba28195eb6b741c97675983a078b0f73
Dennise Catolos <dentolos19@gmail.com>
2024-05-18 22:42:11 +0800

    Updated setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,16 @@
 Set-Location $PSScriptRoot
 
+# Setup Global Editorconfig
+
 Copy-Item -Path "dotfiles\.editorconfig" -Destination (Join-Path $env:USERPROFILE ".editorconfig")
-Copy-Item -Path "dotfiles\profile.ps1" -Destination (Join-Path $env:USERPROFILE "Documents\PowerShell\Microsoft.PowerShell_profile.ps1")
+
+# Setup Powershell Profile
+
+$powershellProfile = (Join-Path $env:USERPROFILE "Documents\PowerShell\Microsoft.PowerShell_profile.ps1")
+$powershellProfileDir = Split-Path -Path $powershellProfile -Parent
+
+if (-not (Test-Path -Path $powershellProfileDir)) {
+    New-Item -ItemType Directory -Path $parentDir | Out-Null
+}
+
+Copy-Item -Path "dotfiles\profile.ps1" -Destination $powershellProfile
