3754a8a3cf692fffbbb8fe963c772c266cef6433
Dennise Catolos <dentolos19@gmail.com>
2025-02-26 20:10:38 +0800

    Updated profile

--- dotfiles/profiles/powershell.profile.ps1
+++ dotfiles/profiles/powershell.profile.ps1
@@ -3,7 +3,7 @@ function ServeFolder {
     & py -m http.server
 }
 
-function BatteryReport() {
+function BatteryReport {
     $outputFile = (Join-Path $env:TEMP "batteryreport.html")
     & powercfg /batteryreport /output $outputFile
     Start-Process $outputFile
@@ -18,8 +18,16 @@ function ClearHistory {
     Write-Host "Your command history has been cleared, please restart your shell!"
 }
 
-Set-Alias -Name "serve" -Value ServeFolder
-Set-Alias -Name "battery" -Value BatteryReport
-Set-Alias -Name "forget" -Value ClearHistory
+function OpenFolder {
+    Start-Process "explorer.exe" .
+}
 
+Set-Alias "serve" -Value ServeFolder
+Set-Alias "battery" -Value BatteryReport
+Set-Alias "forget" -Value ClearHistory
+Set-Alias "open" -Value OpenFolder
+
+$env:FNM_COREPACK_ENABLED = "true"
 $env:COREPACK_ENABLE_AUTO_PIN = "0"
+
+& fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
--- dotfiles/setup.ps1
+++ dotfiles/setup.ps1
@@ -1,18 +1,31 @@
 Set-Location $PSScriptRoot
 
-Write-Host "Installing..."
-
 # Setup Powershell Profile
+
+Write-Host "Installing Profiles..."
 
 $powershellProfileDir = (Join-Path $env:USERPROFILE "Documents\PowerShell")
 $powershellProfileFile = (Join-Path $powershellProfileDir "Microsoft.PowerShell_profile.ps1")
 $powershellConfigFile = (Join-Path $powershellProfileDir "powershell.config.json")
 
 if (-not (Test-Path -Path $powershellProfileDir)) {
-    New-Item -ItemType Directory -Path $powershellProfileDir | Out-Null
+    New-Item $powershellProfileDir -ItemType Directory | Out-Null
 }
 
-Copy-Item -Path "profiles/powershell.profile.ps1" -Destination $powershellProfileFile
-Copy-Item -Path "profiles/powershell.config.json" -Destination $powershellConfigFile
+Copy-Item "profiles/powershell.profile.ps1" $powershellProfileFile
+Copy-Item "profiles/powershell.config.json" $powershellConfigFile
+
+# Install Packages
+
+Write-Host "Installing Packages..."
+
+$packages = @(
+    "Schniz.fnm",
+    "astral-sh.uv"
+)
+
+foreach ($package in $packages) {
+    & winget install $package | Out-Null
+}
 
 Write-Host "Completed!"
